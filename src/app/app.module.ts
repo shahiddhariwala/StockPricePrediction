7727f81041d15804b26e0ea0e9800b53de5fb052
shahiddhariwala <shahid.dhariwala@gmail.com>
2019-08-10 17:13:28 +0530

    First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { StockSelectorComponent } from './stock-selector/stock-selector.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StockSelectorComponent
   ],
   imports: [
     BrowserModule
